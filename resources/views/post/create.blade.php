e20da38ff06a3b1e95e25d77f578e3dbf026cb4b
Hamidin Hidayat <mailnidimah.hamidin@gmail.com>
2017-11-28 18:34:03 +0700

    Add Laravel File Manager

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -33,6 +33,12 @@
 @section('js')
   <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
   <script>
-      CKEDITOR.replace( 'post_body' );
+    var options = {
+      filebrowserImageBrowseUrl: '/laravel-filemanager?type=Images',
+      filebrowserImageUploadUrl: '/laravel-filemanager/upload?type=Images&_token=',
+      filebrowserBrowseUrl: '/laravel-filemanager?type=Files',
+      filebrowserUploadUrl: '/laravel-filemanager/upload?type=Files&_token='
+    };
+      CKEDITOR.replace( 'post_body', options );
   </script>
 @endsection
--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -67,6 +67,13 @@
 @section('js')
   <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
   <script>
-      CKEDITOR.replace( 'post_comment_body' );
+
+    var options = {
+      filebrowserImageBrowseUrl: '/laravel-filemanager?type=Images',
+      filebrowserImageUploadUrl: '/laravel-filemanager/upload?type=Images&_token=',
+      filebrowserBrowseUrl: '/laravel-filemanager?type=Files',
+      filebrowserUploadUrl: '/laravel-filemanager/upload?type=Files&_token='
+    };
+      CKEDITOR.replace( 'post_comment_body', options );
   </script>
 @endsection
